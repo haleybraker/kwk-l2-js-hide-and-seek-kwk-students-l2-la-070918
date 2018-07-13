8c06af04ba1887efaa4fd82e25ac3077e46beed8
haleybraker <haleybraker@gmail.com>
2018-07-13 17:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(selector) {
-  return document.getElementByID(selector)
+  return document.getElementByID(selector).target(selector)
 }
